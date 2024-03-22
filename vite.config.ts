ea7a358bec7457a8e632ebaa1210529959ff9050
archergu <649601376@qq.com>
2024-03-22 16:24:35 +0800

    ci: use github var for build

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/random-box-room/',
   plugins: [
     React(),
     UnoCss(),
