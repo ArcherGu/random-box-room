22203c54bd48dae60e4e5bf797bc5268d204b173
ArcherGu <ArcherGu@users.noreply.github.com>
2024-03-22 07:53:39 +0000

    deploy: 431808dea45466ddcebb7d57f2c05204348df14e

---
+++ assets/batchSamplersUniformGroup-BL_Odgbh.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-CS0br1VQ.js";import{U as o}from"./colorToUniform-CRrSPrlh.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
